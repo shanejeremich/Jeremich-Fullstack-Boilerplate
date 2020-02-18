86da98c5b19791a985954dfbc18621d5be2a43bb
Shane Jeremich <shane.jeremich@gmail.com>
2020-02-18 00:02:32 -0700

    updated react client

--- database/config/dbConfig.js
+++ database/config/dbConfig.js
@@ -1,5 +1,13 @@
+// ./data/dbConfig.js
 const knex = require("knex");
 
-const knexConfig = require("../knexfile");
+const config = require("../../knexfile");
 
-module.exports = knex(knexConfig.development);
+// if the environment variable is not set, default to 'development'
+// this variable is only set when running the "test" npm script using npm run test
+const dbEnv = process.env.DB_ENV || "development";
+
+// the value of dbEnv will be either 'development' or 'testing'
+// we pass it within brackets to select the corresponding configuration
+// from knexfile.js
+module.exports = knex(config[dbEnv]);
